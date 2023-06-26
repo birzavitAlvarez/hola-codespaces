dc3b791bd9b481909d227d284ae28ab840846c07
Birzavit <67138065+birzavitAlvarez@users.noreply.github.com>
2023-06-26 18:55:28 +0000

    basedatos add

---
+++ src/main/java/com/example/holacodespaces/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.holacodespaces.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.holacodespaces.model.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
